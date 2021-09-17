8e260d26ecec73b9548587bdb4ce972a95890af1
Carlos Henrique <rapaiva21@gmail.com>
2021-09-17 17:48:39 -0300

    testes de movimentação de valores entres as contas do banco.

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
-
 cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+cliente1.cpf = 11122233340;
 
 const cliente2 = new Cliente();
-
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-
 contaCorrenteRicardo.agencia = 1001;
-
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(-100);
-const valorSacado = contaCorrenteRicardo.sacar(50);
-
-console.log(valorSacado);
+contaCorrenteRicardo._saldo = 500;
+contaCorrenteRicardo.cliente = cliente1;
 console.log(contaCorrenteRicardo);
+
+const conta2 = new ContaCorrente();
+conta2.agencia = 102;
+conta2._saldo = 200;
+conta2.cliente = cliente2;
+console.log(conta2);
+
+conta2.depositar(300);
+contaCorrenteRicardo.sacar(200);
+
+console.log(contaCorrenteRicardo);
+console.log(conta2);
+
+contaCorrenteRicardo.transferir(200, conta2);
+
+console.log(contaCorrenteRicardo);
+console.log(conta2);
