7f56bd158c29d8a343bdb29e1c956e677814124c
Carlos Henrique <rapaiva21@gmail.com>
2021-09-18 22:44:07 -0300

    Adicionado mais uma conta e um atributo estático para contagem de contas. FIM DO CURSO.

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233340);
 const cliente2 = new Cliente("Alice", 88822233309);
+const cliente3 = new Cliente("José", 12345689);
 
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 const conta2 = new ContaCorrente(102, cliente2);
+const conta3 = new ContaCorrente(102, cliente3);
 
 contaCorrenteRicardo._saldo = 500;
 conta2._saldo = 200;
@@ -18,4 +20,6 @@ contaCorrenteRicardo.depositar(150);
 
 console.log(contaCorrenteRicardo);
 console.log(conta2);
+console.log(conta3);
 
+console.log(ContaCorrente.numeroDeContas);
